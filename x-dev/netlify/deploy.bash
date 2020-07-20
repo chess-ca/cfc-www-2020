f6ea02aca82f4952c2c3e03db7f4186fd7e160b0
Don Parakin <don@weaddvalue.ca>
2020-07-20 12:41:41 -0400

    Test to see if Netlify's build environ has Python (have ideas for build-time scripts).

--- x-dev/netlify/deploy.bash
+++ x-dev/netlify/deploy.bash
@@ -2,29 +2,35 @@
 
 DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DIR_ROOT="$( cd "$DIR_SCRIPT/../.." >/dev/null 2>&1 && pwd )"
+URL_EVENTS="https://docs.google.com/spreadsheets/d/e/2PACX-1vR0OdgsCSYzlRBMOMkLFTHVuIcdXNSz7XQ3KWz4jtJr5fH4wHzDv1HRP3ytG4PwqxAXdTQLjOL0srAB/pub?gid=0&single=true&output=csv"
 
 echo ---- DIR_ROOT = $DIR_ROOT
 
-echo ---- ---- ---- ---- npm install
+echo ---- ---- ---- ---- TASK: npm install
 cd $DIR_ROOT/x-dev
 npm install
 rc=$?
 echo ---- ---- ---- ---- return code: $rc
 if [ $rc -ne 0 ]; then exit $rc; fi
 
-echo ---- ---- ---- ---- npm run webpack:build-prod
+echo ---- ---- ---- ---- TASK: npm run webpack:build-prod
 cd $DIR_ROOT/x-dev
 npm run webpack:build-prod
 rc=$?
 echo ---- ---- ---- ---- return code: $rc
 if [ $rc -ne 0 ]; then exit $rc; fi
 
-echo ---- ---- ---- ---- hugo -d public --gc
+echo ---- ---- ---- ---- TASK: hugo -d public --gc
 cd $DIR_ROOT/hugo
 hugo -d public --gc
 rc=$?
 echo ---- ---- ---- ---- return code: $rc
 if [ $rc -ne 0 ]; then exit $rc; fi
+
+echo ---- ---- ---- ---- TEST: python --version
+python --version
+rc=$?
+echo ---- ---- ---- ---- return code: $rc
 
 
 #=======================================================================
