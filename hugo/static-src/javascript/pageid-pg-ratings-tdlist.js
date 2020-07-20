08fdebf1d8e265542d6e29f80bfe62b025448207
Don Parakin <don@weaddvalue.ca>
2020-07-19 21:56:11 -0400

    Fix to use valid https. Bad cert for swo.webfactional.com.  Switched to cfc-tdlist.parakin.ca and LetsEncrypt.

--- hugo/static-src/javascript/pageid-pg-ratings-tdlist.js
+++ hugo/static-src/javascript/pageid-pg-ratings-tdlist.js
@@ -28,7 +28,7 @@ function ratings_search(event) {
             vue_vm.showing_results = false;
         }
     };
-    var api_url = 'https://swo.webfactional.com/cfc-tdlist/tdlist-api.py'
+    var api_url = 'https://cfc-tdlist.parakin.ca/tdlist-api.py'
         + '?cfc_id=' + this.cfc_id
         + '&first=' + this.first
         + '&last=' + this.last;
