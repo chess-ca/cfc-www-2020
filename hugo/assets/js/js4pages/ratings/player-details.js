c6ec915691d7fb456d4e3692c5309a4e9fd37d0c
Don Parakin <don@weaddvalue.ca>
2021-08-26 17:24:03 -0400

    Fixed Ratings Player Details, when cfc id is not found.

--- hugo/assets/js/js4pages/ratings/player-details.js
+++ hugo/assets/js/js4pages/ratings/player-details.js
@@ -10,26 +10,27 @@ function pre_init(page_data) {
     if (page_data.page_id !== page_id) return;
 
     const pd = page_data;
+    const qvars = get_url_query_vars();
+
     pd.report_is = 'empty';
     pd.dbdate = '';
+    pd.cfc_id = qvars['id'] || '';
     pd.player = {};
     pd.tournaments = [];
     pd.empty_array = [];
-    pd.filter = { t_type: '*' };
+    pd.filter = { t_type: '' };
     pd.player_has_provisional_rating = false;
     pd.event_has_provisional_rating = false;
     pd.filtered_tournaments = filtered_tournaments;
-    pd.show_crosstable = show_crosstable;
+    pd.go_crosstable = go_crosstable;
 }
 
 function post_init(page_data) {
     if (page_data.page_id !== page_id) return;
 
     const pd = page_data;
-    const qvars = get_url_query_vars();
-    const cfc_id = qvars['id'];
-    if ( cfc_id ) {
-        get_player_details(pd, cfc_id);
+    if ( pd.cfc_id ) {
+        get_player_details(pd, pd.cfc_id);
     } else {
         console.error('URL arg "id" is missing.')
         window.location.replace(`/${pd.lang}/ratings/`);
@@ -48,34 +49,32 @@ function get_player_details(page_data, cfc_id) {
         page_data: pd,
         api: `/ratings/player/${cfc_id}`,
         onSuccess: onSuccess,
-        onFail: function(pd) {
-            pd.report_is = 'empty'
-        },
-        onTimeout: function(pd) {
-            pd.report_is = 'timed-out';
-        }
+        onFail: (pd) => { pd.report_is = 'empty'; },
+        onTimeout: (pd) => { pd.report_is = 'timed-out'; }
     });
-}
 
-function onSuccess(page_data, rsp) {
-    const pd = page_data;
-    pd.dbdate = rsp.dbdate || '???';
-    pd.player = rsp.player || {};
-    pd.tournaments = pd.player.tournaments || [];
-    pd.player_has_provisional_rating = (
-        pd.player.rating_hi <= 30 || pd.player.quick_hi <= 30
-    );
-    pd.report_is = 'ready';
+    function onSuccess(page_data, rsp) {
+        const pd = page_data;
+        pd.dbdate = rsp.dbdate || '???';
+        pd.player = rsp.player || {};
+        pd.tournaments = pd.player.tournaments || [];
+        pd.player_has_provisional_rating = (
+            pd.player.rating_hi <= 30 || pd.player.quick_hi <= 30
+        );
+        pd.report_is = 'ready';
 
-    const el_title = document.getElementById('ws-page-title');
-    if (el_title) { el_title.innerText = pd.player.name }
+        if (rsp.player) {
+            const el_title = document.getElementById('ws-page-title');
+            if (el_title) { el_title.innerText = pd.player.name }
+        }
+    }
 }
 
 function filtered_tournaments() {
     const pd = this;
-    pd.event_has_provisional_rating = false;
+    const filter = pd.filter.t_type;
     let list = pd.tournaments.filter(
-        t => (pd.filter.t_type === '*' || pd.filter.t_type === t.type)
+        t => (filter === '' || filter === '*' || filter === t.type)
     );
     pd.event_has_provisional_rating = list.reduce(
         (it_has, t) => it_has || (t.rating_hi < 30), false
@@ -83,7 +82,7 @@ function filtered_tournaments() {
     return list;
 }
 
-function show_crosstable(event_id) {
+function go_crosstable(event_id) {
     const pd = this;
     const cfc_id = pd.player.m_id;
     pd.go(
